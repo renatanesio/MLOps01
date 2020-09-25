eaeca4f6b2ed2bc023449eb6d1a77dc37f546339
Renata Nésio <renatanesio@users.noreply.github.com>
2020-09-25 13:57:17 -0300

    Update train.py

--- train.py
+++ train.py
@@ -1,3 +1,7 @@
+################################
+####### BY ELLE O'BRIEN ########
+################################
+
 import pandas as pd 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
